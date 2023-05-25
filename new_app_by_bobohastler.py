89d868af4e9741087efab0c7fd2d4ec6b205f785
bobohastler <bobohastler@gmail.com>
2023-05-25 21:42:59 +0300

    багато нового

--- new_app_by_bobohastler.py
+++ new_app_by_bobohastler.py
@@ -3,9 +3,21 @@ from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 
-@app.route("/profile/main")
-def profile():
+@app.route("/iceland.html")
+def iceland():
    return render_template("iceland.html")
+
+@app.route("/main.html")
+def main():
+   return render_template("main.html")
+
+@app.route("/about.html")
+def about():
+   return render_template("about.html")
+
+@app.route("/register", methods = ["POST", "GET"])
+def register():
+   return render_template("register.html", title = "Реєстрація", )
 
 if __name__ == "__main__":
    app.run(debug=True)

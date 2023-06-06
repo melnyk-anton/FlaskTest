ca7f9f5f645700034461824278dd469bcae4161c
bobohastler <bobohastler@gmail.com>
2023-06-06 10:44:59 +0300

    коміт

--- practic_for_bobo.py
+++ practic_for_bobo.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+@app.route("/register")
+def change():
+    return render_template("register.html", title = "Register")
 
-if __name__ == "__name__":
+
+if __name__ == "__main__":
     app.run(debug=True)
